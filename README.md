3b2aca12cd331f8a162f643dd2a6d34de698baa9
bturo <beturono@biophys.mpg.de>
2023-06-23 13:01:25 +0200

    Added setup files and instructions

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # cryoCAT
 Contextual Analysis Tools for cryoET and subtomogram averaging
+
+# Install
+
+## Current dependencies
+* To take advantage of all the functions within the package following modules should be installed
+    * scipy
+    * numpy
+    * pandas
+    * skimage
+    * starfile
+    * emfile
+    * mrcfile
+    * matplotlib
+    * seaborn
+    * sklearn
+    * einops
+
+## PIP
+
+Clone the repository and run:
+
+'''
+pip install -e /path/to/your/local/copy/of/cryocat/.
+'''
+
+* Note that the '-e' will install editable version of the package - this is important because you do not have to upgrade it every time something is changed. The only thing you have to do is to pull the repo to have the latest version, all changes will be visible without running pip install upgrade.
+
+
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='cryoCAT',
+    version='0.1.0',    
+    description='Contextual Analysis Tools for CryoET',
+    url='https://github.com/turonova/cryoCAT',
+    author='Beata Turonova',
+    author_email='beata.turonova@gmail.com',
+    license='GPLv3+',
+    packages=['cryocat'],
+    install_requires=['scipy',
+                      'numpy',
+                      'pandas',
+                      'skimage',
+                      'starfile',
+                      'emfile',
+                      'mrcfile',
+                      'matplotlib',
+                      'seaborn',
+                      'sklearn',
+                      'einops'
+                      ],
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
