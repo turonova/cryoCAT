Optimized flipping of subunits, fixed multiple bugs, added new center computation for NPC, fixed bug in loading SG files.

cryocat/cryomap.py
@@ -6,21 +6,63 @@ from scipy.spatial.transform import Rotation as srot
 from scipy.interpolate import interp1d
 
 
-def read(map, transpose=True, data_type=None):
-    if isinstance(map, str):
-        if ".mrc" == map[-4:]:
-            data = mrcfile.open(map).data
-        elif ".rec" == map[-4:]:
-            data = mrcfile.open(map).data
-        elif ".em" == map[-3:]:
-            data = emfile.read(map)[1]
+def read(input_map, transpose=True, data_type=None):
+    """Reads a map file (from the file or numpy array) and returns the data as a numpy array.
+
+    Parameters
+    ----------
+    input_map : str or numpy.ndarray
+        The input map file name or a numpy array containing the map data. The accepted formats are MRC and EM.
+    transpose : bool, optional
+        Whether to transpose the data. Default is True.
+    data_type : numpy.dtype, optional
+        The desired data type of the returned array. If None, the data type is not modified.
+
+    Returns
+    -------
+    numpy.ndarray
+        The map data as a numpy array.
+
+    Raises
+    ------
+    ValueError
+        If the input map file name does not have a valid extension.
+
+    Notes
+    -----
+    This function supports reading map files with the following extensions: .mrc, .rec, .st, .ali, .em.
+
+    If the input_map is a string, the function will attempt to open the file and read the data.
+    If the input_map is a numpy array, it will be directly used as the map data.
+
+    If transpose is True, the data will be transposed using the transpose(2, 1, 0) method.
+
+    If data_type is not None, the data will be cast to the specified data type using the astype method.
+
+    Examples
+    --------
+    >>> data = read("map.mrc")
+    >>> data = read("map.em", transpose=False, data_type=np.float32)
+    >>> data = read(np.random.rand(10, 10, 10))
+    """
+
+    if isinstance(input_map, str):
+        if (
+            input_map.endswith(".mrc")
+            or input_map.endswith(".rec")
+            or input_map.endswith(".st")
+            or input_map.endswith(".ali")
+        ):
+            data = mrcfile.open(input_map).data
+        elif input_map.endswith(".em"):
+            data = emfile.read(input_map)[1]
         else:
-            raise ValueError("The input map file name", map, "is neither em or mrc file!")
+            raise ValueError("The input map file name", input_map, "is neither em or mrc file!")
 
         if transpose:
             data = data.transpose(2, 1, 0)
     else:
-        data = np.array(map)
+        data = np.array(input_map)
 
     if data_type is not None:
         data = data.astype(data_type)
@@ -35,12 +77,15 @@ def write(data_to_write, file_name, transpose=True, data_type=None, overwrite=Tr
     if transpose:
         data_to_write = data_to_write.transpose(2, 1, 0)
 
-    if ".mrc" == file_name[-4:]:
+    if data_to_write.dtype == np.float64:
+        data_to_write = data_to_write.astype(np.float32)
+
+    if file_name.endswith(".mrc") or file_name.endswith(".rec"):
         mrcfile.write(name=file_name, data=data_to_write, overwrite=overwrite)
-    elif ".em" == file_name[-3:]:
+    elif file_name.endswith(".em"):
         emfile.write(file_name, data=data_to_write, overwrite=overwrite)
     else:
-        raise ValueError("The output file name", file_name, "has to end with .mrc or .em!")
+        raise ValueError("The output file name", file_name, "has to end with .mrc, .rec or .em!")
 
 
 def em2mrc(map_name, invert=False, overwrite=True, output_name=None):

cryocat/geom.py
@@ -127,7 +127,7 @@ def spline_sampling(coords, sampling_distance):
         return spline_t
 
 
-def compare_rotations(angles1, angles2, c_symmetry=1):
+def compare_rotations(angles1, angles2, c_symmetry=1, rotation_type="all"):
     """Compare the rotations between two sets of angles.
 
     Parameters
@@ -152,7 +152,16 @@ def compare_rotations(angles1, angles2, c_symmetry=1):
     dist_degrees = angular_distance(angles1, angles2, c_symmetry=c_symmetry)[0]
     dist_degrees_normals, dist_degrees_inplane = cone_inplane_distance(angles1, angles2, c_symmetry=c_symmetry)
 
-    return dist_degrees, dist_degrees_normals, dist_degrees_inplane
+    if rotation_type == "all":
+        return dist_degrees, dist_degrees_normals, dist_degrees_inplane
+    elif rotation_type == "angular_distance":
+        return dist_degrees
+    elif rotation_type == "cone_distance":
+        return dist_degrees_normals
+    elif rotation_type == "in_plane_distance":
+        return dist_degrees_inplane
+    else:
+        raise UserInputError(f"The rotation type {rotation_type} is not supported.")
 
 
 def change_handedness_coordinates(coordinates, dimensions):
@@ -521,6 +530,55 @@ def visualize_rotations(
         ax.set_zlim3d(-radius, radius)
 
     return new_points
+
+
+def angle_between_vectors(vectors1, vectors2):
+    dot_products = np.einsum("ij,ij->i", vectors1, vectors2)
+    norms1 = np.linalg.norm(vectors1, axis=1)
+    norms2 = np.linalg.norm(vectors2, axis=1)
+
+    cosines = dot_products / (norms1 * norms2)
+    radians = np.arccos(np.clip(cosines, -1.0, 1.0))
+    degrees = np.degrees(radians)
+
+    return degrees
+
+
+def compute_pairwise_angles(angles1, angles2, coord1, coord2, axis="z"):
+    angles1 = np.atleast_2d(angles1)
+    angles2 = np.atleast_2d(angles2)
+    rot_angles1 = srot.from_euler("zxz", angles=angles1, degrees=True)
+
+    angles_ref_to_zero = -angles1[:, [2, 1, 0]]  # swap psi and theta -> ["psi", "theta", "phi"]
+    rot_to_zero = srot.from_euler("zxz", angles=angles_ref_to_zero, degrees=True)
+
+    angles2_as_rotations = srot.from_euler("zxz", angles=angles2, degrees=True)
+    # rot_angles2 = rot_to_zero * angles2_as_rotations
+    rot_angles2 = rot_to_zero
+
+    coord_vector = coord2 - coord1
+
+    if axis.endswith("x"):
+        vector = [1, 0, 0]
+    elif axis.endswith("y"):
+        vector = [0, 1, 0]
+    elif axis.endswith("z"):
+        vector = [0, 0, 1]
+    else:
+        raise UserInputError(f"Invalid axis epcification: {axis}. Allowed inputs are x, -x, y, -y, z, -z!")
+
+    if axis.startswith("-"):
+        vector = vector * -1
+
+    rot_vectors1 = rot_angles1.apply(vector)
+    rot_vectors2 = rot_angles2.apply(coord_vector)
+
+    angles = angle_between_vectors(rot_vectors1, rot_vectors2)
+    ##nn_rotations = srot.concatenate(nn_rotations)
+    ##points_on_sphere = geom.visualize_rotations(nn_rotations, plot_rotations=False)
+    ##angles = nn_rotations.as_euler("zxz", degrees=True)
+
+    return angles
 
 
 def visualize_angles(angles, plot_rotations=True, color_map=None):