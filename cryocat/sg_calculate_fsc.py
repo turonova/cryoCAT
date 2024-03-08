fbbf2104109d597a71aa201b5beea7e9bc4269db
makubans <makubans@biophys.mpg.de>
2024-03-08 16:27:42 +0100

    240308_16:30

--- cryocat/sg_calculate_fsc.py
+++ cryocat/sg_calculate_fsc.py
@@ -13,21 +13,40 @@ def load_for_fsc(refA_name: str,
                 refB_name: str,
                 fsc_mask_name: Optional[str]=None,
                 symmetry: Optional[str]='C1'):#write_outputs, pixelsize)
+    """Reads 2 reference maps to compare, reads or creates the mask, applies input symmetry and mask.
+    
+    Parameters
+    ----------
+    refA_name, refB_name: str or (NP NDARRAY - from cryomap.read doc?!??). Paths to maps to compare.
+    fsc_mask_name: str, np.ndarray?!?!??!, default None. Path to mask to be applied. If none, creates a mask of ones.
+    symmetry: str. Map symmetry to apply. Default C1.
+
+    Returns
+    -------
+
+    Raises
+    ------
+    UserInputError
+        If the input map sizes do not match each other and/or mask, if provided.
+
+    """
+
     refA = cryocat.cryomap.read(refA_name)
     refB = cryocat.cryomap.read(refB_name)
     if len(refA[0]) == len(refB[0]):            
         if fsc_mask_name is not None:
             fsc_mask=cryocat.cryomap.read(fsc_mask_name)
+            if len(refA[0]) != len(fsc_mask[0]):
+                raise UserInputError('Provided mask does not match the size of the map.')
         else:
-            #print("Creating an empty square mask...")
-            #fsc_mask=fsc_mask_name
+            print("Creating an empty square mask...")
             fsc_mask=np.ones(cryocat.cryomask.get_correct_format(len(refA),len(refA)))
-            print(np.linalg.eigvals(fsc_mask))
-            ## TODO lengths of inputs - or error -> and test for error
-        ##APPLY MASKS
     else:
         raise UserInputError('Sizes of volumes to compare do not match!')
-        
-    return refA, refB, fsc_mask, symmetry
+### TODO should the ref be centered/centered under the mask?
+    mrefA=np.multiply(refA, fsc_mask)
+    mrefB=np.multiply(refB, fsc_mask)
+## TODO APPLY symmetry?
+    return refA, refB, fsc_mask, mrefA, mrefB, symmetry
 
 #def 
--- tests/test_fsc.py
+++ tests/test_fsc.py
@@ -10,23 +10,36 @@ from cryocat import cryomap
 from cryocat import cryomask
 from cryocat import sg_calculate_fsc as fsc
 
-# @pytest.mark.parametrize('refA_name', ['/Volumes/msdata/makubans/fsc_python/npc_ref_A_px_1_inv.em'],
-#                         'refB_name', ['/Volumes/msdata/makubans/fsc_python/npc_ref_B_px_1_inv.em'],
-#                         'fsc_mask_name', ['/Volumes/msdata/makubans/fsc_python/sphere_r10_g2_bx32.em'])
+### For sh. and giggles
+@pytest.mark.parametrize('refA_name',[('./tests/test_data/npc_ref_A_px_1_inv.em')])
+@pytest.mark.parametrize('refB_name', [('./tests/test_data/npc_ref_B_px_1_inv.em')])
+@pytest.mark.parametrize('fsc_mask_name', [('./tests/test_data/sphere_r10_g2_bx32.em')])
 
+def test_load_in(refA_name, refB_name, fsc_mask_name):
+    inputs = fsc.load_for_fsc(refA_name,
+                        refB_name,
+                        fsc_mask_name)
+    inputs_cryocat = [cryomap.read(refA_name),
+    cryomap.read(refB_name),
+    cryomap.read(fsc_mask_name)]
+    assert np.allclose(inputs[0:2], inputs_cryocat[0:2])## [0-2] bc load_for_fsc output being a tuple (shapes)
 
-#class UnitTestsMethods(unittest.TestCase):
-def test_inputs():
-    inputs = fsc.load_for_fsc(refA_name='/Volumes/msdata/makubans/fsc_python/npc_ref_A_px_1_inv.em',
-                        refB_name='/Volumes/msdata/makubans/fsc_python/npc_ref_B_px_1_inv.em',
-                        fsc_mask_name='/Volumes/msdata/makubans/fsc_python/sphere_r10_g2_bx32.em')
-    inputs_cryocat = [cryomap.read('/Volumes/msdata/makubans/fsc_python/npc_ref_A_px_1_inv.em'),
-    cryomap.read('/Volumes/msdata/makubans/fsc_python/npc_ref_B_px_1_inv.em'),
-    cryomap.read('/Volumes/msdata/makubans/fsc_python/sphere_r10_g2_bx32.em')]
-    assert np.allclose(inputs[0:2], inputs_cryocat[0:2])## [0-2] bc load_for_fsc shapes
-
-def test_nomask():
-    inputs =  fsc.load_for_fsc(refA_name='/Volumes/msdata/makubans/fsc_python/npc_ref_A_px_1_inv.em',
-                        refB_name='/Volumes/msdata/makubans/fsc_python/npc_ref_B_px_1_inv.em',
+def test_none_mask():
+    inputs =  fsc.load_for_fsc('./tests/test_data/npc_ref_A_px_1_inv.em',
+                        './tests/test_data/npc_ref_B_px_1_inv.em',
                         fsc_mask_name=None)
     assert np.array_equal(inputs[2], inputs[2].T)
+
+def test_apply_none_mask():
+    inputs =  fsc.load_for_fsc('./tests/test_data/npc_ref_A_px_1_inv.em',
+                        './tests/test_data/npc_ref_B_px_1_inv.em',
+                        fsc_mask_name=None)
+    assert np.array_equal(inputs[0], inputs[3]) & np.array_equal(inputs[1], inputs[4])
+
+def test_mask_does_something():
+    inputs =  fsc.load_for_fsc('./tests/test_data/npc_ref_A_px_1_inv.em',
+                        './tests/test_data/npc_ref_B_px_1_inv.em',
+                        './tests/test_data/sphere_r10_g2_bx32.em')
+    assert np.less_equal(inputs[0], inputs[3]).any() & np.less_equal(inputs[1], inputs[4]).any()
+
+    #pass
