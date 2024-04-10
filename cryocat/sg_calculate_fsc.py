4a4c5aa095f090ac68ca7001509476d2c2945f82
makubans <makubans@biophys.mpg.de>
2024-04-10 10:38:41 +0200

    10042024

--- cryocat/sg_calculate_fsc.py
+++ cryocat/sg_calculate_fsc.py
@@ -2,7 +2,8 @@
 # import mrcfile
 import numpy as np
 # from typing import Optional
-
+import sys
+from matplotlib import pyplot as plt
 import cryocat
 from cryocat.exceptions import UserInputError
 
@@ -36,6 +37,7 @@ def load_for_fsc(refA_name,
 
     refA = cryocat.cryomap.read(refA_name)
     refB = cryocat.cryomap.read(refB_name)
+    # mask the volumes; in case no mask was provided create an empty mask the size of the input box
     if len(refA[0]) == len(refB[0]):            
         if fsc_mask_name is not None:
             fsc_mask=cryocat.cryomap.read(fsc_mask_name)
@@ -46,18 +48,15 @@ def load_for_fsc(refA_name,
             fsc_mask=np.ones(cryocat.cryomask.get_correct_format(len(refA),len(refA)))
     else:
         raise UserInputError('Sizes of volumes to compare do not match!')
-### TODO should I normalise under mask?
     mrefA=np.multiply(refA, fsc_mask)
     mrefB=np.multiply(refB, fsc_mask)
-## TODO APPLY symmetry?
-    # B: yes, a function "symmetrize" should be called here, but the function itself should be in cryomap
+## TODO APPLY symmetry
     return refA, refB, fsc_mask, mrefA, mrefB, symmetry
   
 
 def calculate_fsc(mrefA, mrefB):
-    ### apply FFT to the whole box with no padding over every axis (TODO introduce cutoff); shift 0 to the middle of the box
-    ### TODO normalisation??? "The default normalization ("backward") has the direct (forward) transforms unscaled and the inverse (backward) transforms scaled by 1/n"
-    # B: should not be necessary to normalize here
+    ### apply FFT to the whole box with no padding over every axis (TODO introduce cutoff); shift 0 to the middle of the box TODO check that it is indeed the case
+    ### "The default normalization ("backward") has the direct (forward) transforms unscaled and the inverse (backward) transforms scaled by 1/n"
     mftA = np.fft.fftshift(np.fft.fftn(mrefA))
     mftB = np.fft.fftshift(np.fft.fftn(mrefB))
 
@@ -67,7 +66,7 @@ def calculate_fsc(mrefA, mrefB):
     fsc_B = np.multiply(mftB, np.conjugate(mftB))  ## = |F2|^2
 
     ### calculate shells
-    ### TODO test for odd and even len numbers
+    ### TODO test for odd and even len numbers - odd numbers should not exist
     # B: this is the distance array - should go most likely to mathutils or somewhere as a standalone function -> I did in mathutils, needs testing first
     shell_grid = np.arange(math.floor(-len(mrefA[0]) / 2), math.ceil(len(mrefA[0]) / 2), 1)
     xv, yv, zv = shell_grid, shell_grid, shell_grid
@@ -88,53 +87,48 @@ def calculate_fsc(mrefA, mrefB):
         
         # Write out linearized shell mask
         shell_mask.append(temp_mask.astype(int))
-    ##########shell_mask.append(distance_v.astype(int)) for (distance_v >= shell_edge - 1) and (distance_v < shell_edge) for shell_edge in range(1, max_n_shell+1)
 
     AB_cc_array = []    # Complex conjugate of A and B
     intA_array = []     # Intenisty of A
     intB_array = []     # Intenisty of B
 
+    full_fsc = [] 
 
-    full_fsc_r = [] ### DELETE LATER
-    full_fsc = np.empty([1,len(shell_mask)])
-
-    import sys
-    import numpy
-    numpy.set_printoptions(threshold=sys.maxsize)
+    # np.set_printoptions(threshold=sys.maxsize) ###????
     for r in range(len(shell_mask)):
         
-        print("r", r, "mask",  shell_mask[r][16][16])#, np.where(np.multiply(shell_mask[r],fsc_denominator)))
+        # print("r", r, "mask",  shell_mask[r][16][16])#, np.where(np.multiply(shell_mask[r],fsc_denominator)))
        
         # print("previous 893869. 893547. 894000. 894962. 893869. 891337.")
         # print(np.multiply(shell_mask[r][16][16], fsc_denominator[16][16]))
         # print(np.where(shell_mask[r][16][16]*fsc_denominator[16][16]))
         # print(np.real(np.sum(shell_mask[r][16][16]*fsc_denominator[16][16])))
 
-
         AB_cc_array.append(np.sum(shell_mask[r] * fsc_denominator))
-        print(np.sum(shell_mask[r] * fsc_denominator))
+        # print(np.sum(shell_mask[r] * fsc_denominator))
         intA_array.append(np.sum(shell_mask[r] * fsc_A))
-        print(np.sum(shell_mask[r] * fsc_A))
+        # print(np.sum(shell_mask[r] * fsc_A))
         intB_array.append(np.sum(shell_mask[r] * fsc_B))
-        print(np.sum(shell_mask[r] * fsc_B))
+        # print(np.sum(shell_mask[r] * fsc_B))
         #print("fsc_r", AB_cc_array/np.sqrt(np.multiply(intA_array, intB_array)))
-        print(type(AB_cc_array), type(intA_array), type(intB_array))
+        # print(type(AB_cc_array), type(intA_array), type(intB_array))
 
-        full_fsc_r.append(np.real(AB_cc_array[r]/np.sqrt(np.multiply(intA_array[r], intB_array[r]))))
-    print('full_fsc_r', full_fsc_r) ## this one looks better whilst plotting; remember nan is the first value???
+        full_fsc.append(np.real(np.divide(AB_cc_array[r], np.sqrt(np.multiply(intA_array[r], intB_array[r])))))
+    # print('full_fsc_r', full_fsc_r) ## this one looks better whilst plotting; remember nan is the first value???
     # full_fsc = np.real(AB_cc_array/np.sqrt(np.multiply(intA_array, intB_array)))
     # full_fsc.reshape(17,1)
-    print("full_fsc", full_fsc)
+    # print("full_fsc", full_fsc)
     # full_fsc = np.real(AB_cc_array/math.sqrt(intA_array * intB_array))
-    return full_fsc, full_fsc_r
+    
 
+    return full_fsc
 
 def phase_randomised_reference(mft,
                             fourier_cutoff=5):
     amp, phase = np.absolute(mft), np.angle(mft)
     pr_phase = phase.copy()
     
-   ### TODO so far gives error
+   # ## TODO so far gives error:
    # ## distance_v = cryocat.mathutils.distance_array(mft)
     shell_grid = np.arange(math.floor(-len(mft[0]) / 2), math.ceil(len(mft[0]) / 2), 1)
     xv, yv, zv = shell_grid, shell_grid, shell_grid
