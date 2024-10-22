5f9a2166c77c07b3c5a99c4aa1cfb637ce23c11c
Jiasuiliu <jiasuiliu@outlook.com>
2024-10-22 13:36:25 +0200

    Fixed a bug in ellipsoid_shell_mask

--- cryocat/cryomask.py
+++ cryocat/cryomask.py
@@ -622,8 +622,8 @@ def ellipsoid_shell_mask(mask_size, shell_thickness, radii, center=None, gaussia
 
     shell_thickness = shell_thickness / 2
 
-    e1 = ellipsoid_mask(mask_size, radii == radii + shell_thickness, center=center)
-    e2 = ellipsoid_mask(mask_size, radius=radii - shell_thickness, center=center)
+    e1 = ellipsoid_mask(mask_size, radii = radii + shell_thickness, center=center)
+    e2 = ellipsoid_mask(mask_size, radii = radii - shell_thickness, center=center)
 
     shell_mask = e1 - e2
 
