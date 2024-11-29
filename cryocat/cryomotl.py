Added more functions for surfaces. Fixed bug in dose computation.

cryocat/cryomotl.py
@@ -22,6 +22,7 @@ from math import ceil
 from matplotlib import pyplot as plt
 
 from scipy.spatial import KDTree
+from sklearn.neighbors import KDTree as snKDTree
 from scipy.spatial.transform import Rotation as rot
 
 
@@ -270,6 +271,80 @@ class Motl:
             cleaned_df = pd.concat((cleaned_df, feature_m.df.iloc[temp_keep, :]), ignore_index=True)
 
         self.df = cleaned_df
+
+    def clean_by_distance_to_points(
+        self, points, radius_in_voxels, feature_id="tomo_id", inplace=True, output_file=None
+    ):
+        """Cleans the motl by removing points that are within a specified radius of any point in a the provided dataframe
+        with points.
+        
+        Parameters
+        ----------
+        points : DataFrame
+            DataFrame containing the coordinates of points to check against. It has to contain column specified by the
+            param feature_id and also columns x,y,z.
+        radius_in_voxels : float
+            The radius within which points will be considered for removal, in voxel units.
+        feature_id : str, default="tomo_id"
+            The column name that identifies how the particles in motl should be grouped. Defaults to 'tomo_id'.
+        inplace : bool, default=True
+            If True, modifies the motl in place. If False, returns a new Motl object. Defaults to True.
+        output_file : str or None, optional
+            If specified, the cleaned Motl object will be written to this file. Defaults to None.
+        
+        Returns
+        -------
+        Motl or None
+            If inplace is False, returns a new Motl object containing the cleaned DataFrame. Otherwise, returns None.
+        
+        Notes
+        -----
+        The function uses a KDTree for efficient spatial queries, which can significantly speed up the process of finding
+        nearby points. The function assumes that the input motl and the points DataFrame have columns 'x', 'y', and 'z'
+        that represent coordinates and also columns specified by feature_id.
+        """
+        
+        # Parse tomograms
+        features = self.get_unique_values(feature_id)
+
+        # Initialize clean motl
+        cleaned_df = pd.DataFrame()
+
+        # Loop through and clean
+        for f in features:
+            # Parse tomogram
+            feature_m = self.get_motl_subset(f, feature_id=feature_id, reset_index=True)
+
+            # Parse positions
+            coord1 = feature_m.get_coordinates()
+            coord2 = points.loc[points[feature_id] == f, ["x", "y", "z"]].values
+
+            # Create a KDTree from coord1
+            tree = KDTree(coord1)
+
+            # Query points from coord2 within the radius
+            indices_to_remove = set()  # Use a set to store unique indices
+            for point in coord2:
+                indices = tree.query_ball_point(point, r=radius_in_voxels)  # Returns indices as array
+                indices_to_remove.update(indices)  # Add indices to the set
+
+            # Convert to a sorted list for consistent ordering
+            indices_to_remove = sorted(indices_to_remove)
+            cfm = feature_m.df.drop(index=indices_to_remove)
+            cleaned_df = pd.concat([cleaned_df, cfm], ignore_index=True)
+
+        cleaned_df.reset_index(drop=True, inplace=True)
+        cleaned_motl = Motl(cleaned_df)
+
+        if output_file:
+            cleaned_motl.write_out(output_file)
+
+        print(f"{self.df.shape[0]-cleaned_motl.df.shape[0]} particles were removed.")
+
+        if inplace:
+            self.df = cleaned_df
+        else:
+            return cleaned_motl
 
     def clean_by_tomo_mask(self, tomo_list, tomo_masks, inplace=True, output_file=None):
         """Removes particles from the motive list based on provided tomomgram masks.
@@ -1533,7 +1608,7 @@ class Motl:
         old_center = np.array(input_mask.shape) / 2
         mask_center = cryomask.get_mass_center(input_mask)  # find center of mask
         shifts = mask_center - old_center  # get shifts
-
+        print(shifts)
         # change shifts in the motl accordingly
         motl = motl_orig.shift_positions(shifts, inplace=False)
         motl.update_coordinates()
@@ -3544,6 +3619,19 @@ class ModMotl(Motl):
 
             return result
 
+        def check_tomo_id_type(df):
+            if df["mod_id"].apply(lambda x: isinstance(x, str)).all():
+                # If all values are strings, extract digits and convert to integers
+                df["mod_id"] = df["mod_id"].str.extract("(\d+)")[0].astype(int)
+            elif df["mod_id"].apply(lambda x: isinstance(x, int)).all():
+                # If all values are integers, do nothing or keep as is
+                pass
+            else:
+                raise ValueError("Column contains mixed types or unexpected data.")
+
+            return df
+
+        mod_df = check_tomo_id_type(mod_df)
         self.mod_df = mod_df
 
         contours_per_object = mod_df["object_id"].value_counts(sort=False)
@@ -3562,6 +3650,14 @@ class ModMotl(Motl):
         elif len(set(points_per_contour)) == 1:  # each contour has the same number of points
             if (points_per_contour == 2).all():
                 points = mod_df.groupby(["object_id", "contour_id"]).apply(subtract_rows).reset_index(drop=True)
+        else:
+            points = {
+                "coord": mod_df[["x", "y", "z"]].values,
+                "object_id": mod_df["object_id"].values,
+                "tomo_id": mod_df["mod_id"].astype(int).values,
+                "geom2": mod_df["contour_id"].values,
+                "geom5": mod_df["object_radius"].values,
+            }
 
         self.fill(points)
         self.df["subtomo_id"] = np.arange(1, self.df.shape[0] + 1)

cryocat/geom.py
@@ -5,6 +5,7 @@ from cryocat.exceptions import UserInputError
 import matplotlib.pyplot as plt
 import os
 from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.interpolate import splprep, splev
 
 ANGLE_DEGREES_TOL = 10e-12
 
@@ -265,13 +266,37 @@ def cone_distance(input_rot1, input_rot2):
     return cone_angle
 
 
+def get_axis_from_rotation(input_rotation, axis="z"):
+
+    matrix_rep = input_rotation.as_matrix()
+
+    axes_dict = {"x": 0, "y": 1, "z": 2}
+
+    if matrix_rep.shape == (3, 3):  # Single (3, 3) matrix
+        ret_axis = matrix_rep[:, axes_dict[axis]]  # Extract column 1 for a single matrix
+    elif matrix_rep.shape[1:] == (3, 3):  # Multiple (N, 3, 3) matrices
+        ret_axis = matrix_rep[:, :, axes_dict[axis]]  # Extract column 1 for each (N, 3, 3) matrix
+    else:
+        raise ValueError("Input must be valid scipy rotation object.")
+
+    return ret_axis
+
+
 def inplane_distance(input_rot1, input_rot2, convention="zxz", degrees=True, c_symmetry=1):
+
+    y_axis1 = get_axis_from_rotation(input_rot1, axis="y")
+    y_axis2 = get_axis_from_rotation(input_rot2, axis="y")
+
+    inplane_angle = angle_between_n_vectors(y_axis1, y_axis2)
+
+    return inplane_angle
+    """
     phi1 = np.array(input_rot1.as_euler(convention, degrees=degrees), ndmin=2)[:, 0]
     phi2 = np.array(input_rot2.as_euler(convention, degrees=degrees), ndmin=2)[:, 0]
 
     # Remove flot precision errors during conversion
-    phi1 = np.where(phi1 < ANGLE_DEGREES_TOL, 0.0, phi1)
-    phi2 = np.where(phi2 < ANGLE_DEGREES_TOL, 0.0, phi2)
+    phi1 = np.where(abs(phi1) < ANGLE_DEGREES_TOL, 0.0, phi1)
+    phi2 = np.where(abs(phi2) < ANGLE_DEGREES_TOL, 0.0, phi2)
 
     # From Scipy the phi is from [-180,180] -> change to [0.0,360]
     phi1 += 180.0
@@ -288,6 +313,7 @@ def inplane_distance(input_rot1, input_rot2, convention="zxz", degrees=True, c_s
     inplane_angle = np.where(inplane_angle > 180.0, np.abs(inplane_angle - 360.0), inplane_angle)
 
     return inplane_angle
+    """
 
 
 def cone_inplane_distance(input_rot1, input_rot2, convention="zxz", degrees=True, c_symmetry=1):
@@ -773,6 +799,71 @@ def area_triangle(coords):
     triangles = np.cross(coords[:, 1] - coords[:, 0], coords[:, 2] - coords[:, 0])
     # The norm of the cross product of two sides is twice the area
     return np.linalg.norm(triangles) / 2
+
+
+def ray_ellipsoid_intersection_3d(point, normal, ellipsoid_params):
+
+    # Extract line parameters
+    x, y, z = point[0], point[1], point[2]
+    n1, n2, n3 = normal[0], normal[1], normal[2]
+
+    # Extract ellipsoid parameters
+    a, b, c, d, e, f, g, h, i, j = ellipsoid_params
+
+    # Calculate coefficients A, B, C for the quadratic equation
+    A = a * n1**2 + b * n2**2 + c * n3**2 + 2 * d * n1 * n2 + 2 * e * n1 * n3 + 2 * f * n3 * n2
+    B = 2 * (
+        a * x * n1
+        + b * y * n2
+        + c * z * n3
+        + d * x * n2
+        + d * y * n1
+        + e * x * n3
+        + e * z * n1
+        + f * z * n2
+        + f * y * n3
+        + g * n1
+        + h * n2
+        + i * n3
+    )
+    C = j + a * x**2 + b * y**2 + c * z**2 + 2 * (i * z + h * y + g * x + d * x * y + e * x * z + f * z * y)
+
+    # Discriminant
+    D = B**2 - 4 * A * C
+
+    # Initialize results
+    p1, p2 = None, None
+    is_inside = False
+
+    if D < 0:  # No intersection
+        p1, p2 = np.nan, np.nan
+        d1, d2 = np.nan, np.nan
+    elif D == 0:  # One intersection point
+        t1 = -B / (2 * A)
+        p1 = point + t1 * normal
+        d1 = np.linalg.norm(p1 - point, axis=1)
+        p2 = np.nan
+        d2 = np.nan
+    else:  # Two intersection points
+        t1 = (-B + np.sqrt(D)) / (2 * A)
+        t2 = (-B - np.sqrt(D)) / (2 * A)
+
+        p1 = point + t1 * normal
+        p2 = point + t2 * normal
+
+        ps = np.column_stack((p1, p2))
+        distances = np.linalg.norm(ps.T - point, axis=1)
+        pi = np.argmin(distances)
+
+        p1 = ps[:, pi]
+        p2 = ps[:, 1 - pi]
+        d1 = distances[pi]
+        d2 = distances[1 - pi]
+
+        if np.sign(t1) != np.sign(t2):
+            is_inside = True
+
+    return p1, p2, d1, d2, is_inside
 
 
 def ray_ray_intersection_3d(starting_points, ending_points):
@@ -1325,6 +1416,25 @@ def normalize_vector(vector):
     return vector / np.linalg.norm(vector)
 
 
+def normalize_vectors(v):
+    # Normalize each vector, handling both single vectors and arrays of vectors
+    norm = np.linalg.norm(v, axis=-1, keepdims=True)
+    return v / norm
+
+
+def angle_between_n_vectors(v1, v2):
+    # Ensure both vectors are normalized
+    v1_u = normalize_vectors(v1)
+    v2_u = normalize_vectors(v2)
+
+    # Compute dot product element-wise, handling both (3,) and (N, 3) shapes
+    dot_product = np.einsum("ij,ij->i", v1_u, v2_u) if v1.ndim > 1 else np.dot(v1_u, v2_u)
+
+    # Clip values to avoid numerical errors outside the valid range for arccos
+    angle = np.degrees(np.arccos(np.clip(dot_product, -1.0, 1.0)))
+    return angle
+
+
 def vector_angular_distance(v1, v2):
     """Calculate the angular distance between two vectors in degrees.
 
@@ -1404,3 +1514,33 @@ def vector_angular_distance_signed(u, v, n=None):
         return np.arctan2(np.linalg.norm(np.cross(u, v)), np.dot(u, v))
     else:
         return np.arctan2(np.dot(n, np.cross(u, v)), np.dot(u, v))
+
+
+def oversample_spline(coords, target_spacing):
+    """
+    Fit a spline through 3D coordinates and oversample so that the distance between points is approximately `target_spacing`.
+
+    Parameters:
+        coords (np.ndarray): Array of shape (n, 3) representing the input points.
+        target_spacing (float): Desired distance between points on the spline.
+
+    Returns:
+        np.ndarray: Oversampled coordinates along the spline.
+    """
+    # Fit a parametric spline to the data
+    tck, u = splprep(coords.T, s=0)  # `s=0` ensures an exact fit to the input points
+
+    # Compute cumulative arc length
+    distances = np.sqrt(np.sum(np.diff(coords, axis=0) ** 2, axis=1))
+    total_length = np.sum(distances)
+
+    # Determine number of samples based on target spacing
+    num_samples = int(total_length / target_spacing) + 1
+
+    # Generate evenly spaced parameter values along the spline
+    u_fine = np.linspace(0, 1, num_samples)
+
+    # Evaluate the spline to get oversampled points
+    oversampled_points = np.array(splev(u_fine, tck)).T
+
+    return oversampled_points

cryocat/tiltstack.py
@@ -9,48 +9,128 @@ import warnings
 from functools import wraps
 
 
-def suppress_warnings(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        with warnings.catch_warnings():
-            warnings.filterwarnings("ignore")
-            result = func(*args, **kwargs)
-        return result
+class TiltStack:
 
-    return wrapper
+    def __init__(self, tilt_stack, input_order="xyz", output_order="xyz"):
+
+        if not isinstance(tilt_stack, np.ndarray):  # if loading necessary, load in zyx
+            self.data = cryomap.read(tilt_stack, transpose=False)
+        else:
+            self.data = tilt_stack.copy()
+            if input_order == "xyz":
+                self.data = self.data.transpose(2, 1, 0)
+
+        self.data_type = self.data.dtype
+
+        self.input_order = input_order
+        self.current_order = "zyx"
+        self.output_order = output_order
+
+        self.n_tilts, self.height, self.width = self.data.shape
+
+    def write_out(self, output_file, new_data=None):
+
+        if output_file:
+            cryomap.write(new_data or self.data, output_file, data_type=self.data_type, transpose=False)
+
+    def correct_order(self, new_data=None):
+
+        return_data = new_data or self.data
+
+        if return_data.dtype != self.data_type:
+            return_data = return_data.astype(self.data_type)
+
+        if self.current_order != self.output_order:
+            return return_data.transpose(2, 1, 0)
+        else:
+            return return_data
 
 
-def bin(tilt_stack, binning_factor):
+def crop(tilt_stack, new_width=None, new_height=None, output_file=None, input_order="xyz", output_order="xyz"):
 
-    # binned_stack = np.zeros((tilt_stack.shape[0],tilt_stack.shape[1],tilt_stack.shape[0]
+    ts = TiltStack(tilt_stack=tilt_stack, input_order=input_order, output_order=output_order)
 
-    # for z in range(tilt_stack.shape[0]):
+    new_width = new_width or ts.width
+    new_height = new_height or ts.height
 
-    #    binned_stack[z, :, :] = downscale_local_mean(tilt_stack[z, :, :], (1, binning_factor, binning_factor))
+    # Calculate the center of the original array
+    center_w, center_h = ts.width // 2, ts.height // 2
 
-    binned_stack = downscale_local_mean(tilt_stack, (1, binning_factor, binning_factor))
-    print(tilt_stack.dtype)
-    return binned_stack.astype(tilt_stack.dtype)
+    # Calculate the cropping indices
+    start_w = int(center_w - int(new_width) // 2)
+    end_w = int(start_w + int(new_width))
+
+    start_h = int(center_h - int(new_height) // 2)
+    end_h = int(start_h + int(new_height))
+
+    # crop the actual images
+    ts.data = ts.data[:, start_h:end_h, start_w:end_w]
+
+    ts.write_out(output_file)
+
+    return ts.correct_order()
 
 
-def equalize_histogram(tilt_stack, eh_method="contrast_stretching"):
+def sort_tilts_by_angle(tilt_stack, input_tilts, output_file=None, input_order="xyz", output_order="xyz"):
 
-    equalized_titls = np.zeros(tilt_stack.shape)
+    ts = TiltStack(tilt_stack=tilt_stack, input_order=input_order, output_order=output_order)
 
-    for z in range(tilt_stack.shape[0]):
+    tilt_angles = ioutils.tlt_load(input_tilts, sort_angles=False)
+    sorted_indices = np.argsort(tilt_angles)
+
+    ts.data = ts.data[sorted_indices, :, :]
+    ts.write_out(output_file)
+
+    return ts.correct_order()
+
+
+def remove_tilts(
+    tilt_stack, idx_to_remove, numbered_from_1=True, output_file=None, input_order="xyz", output_order="xyz"
+):
+
+    ts = TiltStack(tilt_stack=tilt_stack, input_order=input_order, output_order=output_order)
+
+    idx_to_remove = np.asarray(idx_to_remove)
+    if numbered_from_1:
+        idx_to_remove = idx_to_remove - 1
+
+    ts.data = np.delete(ts.data, idx_to_remove, axis=0)
+    ts.write_out(output_file)
+
+    return ts.correct_order()
+
+
+def bin(tilt_stack, binning_factor, output_file=None, input_order="xyz", output_order="xyz"):
+
+    ts = TiltStack(tilt_stack=tilt_stack, input_order=input_order, output_order=output_order)
+    ts.data = downscale_local_mean(ts.data, (1, binning_factor, binning_factor))
+    ts.write_out(output_file)
+
+    return ts.correct_order()
+
+
+def equalize_histogram(
+    tilt_stack, eh_method="contrast_stretching", output_file=None, input_order="xyz", output_order="xyz"
+):
+
+    ts = TiltStack(tilt_stack=tilt_stack, input_order=input_order, output_order=output_order)
+
+    for z in ts.n_tilts:
         if eh_method == "contrast_stretching":
-            p2, p98 = np.percentile(tilt_stack[z, :, :], (2, 98))
-            equalized_titls[z, :, :] = exposure.rescale_intensity(tilt_stack[z, :, :], in_range=(p2, p98))
+            p2, p98 = np.percentile(ts.data[z, :, :], (2, 98))
+            ts.data[z, :, :] = exposure.rescale_intensity(ts.data[z, :, :], in_range=(p2, p98))
         elif eh_method == "equalization":
             # Equalization
-            equalized_titls[z, :, :] = exposure.equalize_hist(tilt_stack[z, :, :])
+            ts.data[z, :, :] = exposure.equalize_hist(ts.data[z, :, :])
         elif eh_method == "adaptive_eq":
             # Adaptive Equalization
-            equalized_titls[z, :, :] = exposure.equalize_adapthist(tilt_stack[z, :, :], clip_limit=0.03)
+            ts.data[z, :, :] = exposure.equalize_adapthist(ts.data[z, :, :], clip_limit=0.03)
         else:
             raise ValueError(f"The {eh_method} is not known!")
 
-    return equalized_titls.astype(tilt_stack.dtype)
+    ts.write_out(output_file)
+
+    return ts.correct_order()
 
 
 def calculate_total_dose_batch(tomo_list, prior_dose_file_format, dose_per_image, output_file_format):
@@ -70,52 +150,42 @@ def calculate_total_dose(prior_dose, dose_per_image):
     return total_dose
 
 
-def dose_filter(mrc_file, pixel_size, total_dose, output_file=None, return_data_order="xyz"):
+def dose_filter(tilt_stack, pixel_size, total_dose, output_file=None, input_order="xyz", output_order="xyz"):
     # Input: mrc_file or path to it
     #        pixelsize: float, in Angstroms
     #        total_dose: ndarray or path to the .csv, .txt, or .mdoc file
     #        return_data_order: by default x y z (x,y,n_tilts), for napari compatible view use "zyx"
 
     # temporarily here until this function exist as an entry point on command line
-    pixel_size = float(pixel_size)
 
-    stack_data = cryomap.read(mrc_file)
+    ts = TiltStack(tilt_stack=tilt_stack, input_order=input_order, output_order=output_order)
+    pixel_size = float(pixel_size)
     total_dose = ioutils.total_dose_load(total_dose)
 
-    imgs_x = stack_data.shape[0]
-    imgs_y = stack_data.shape[1]
-    n_tilt_imgs = stack_data.shape[2]
-
     # Precalculate frequency array
-    frequency_array = np.zeros((imgs_x, imgs_y))
-    cen_x = imgs_x // 2  # Center for array is half the image size
-    cen_y = imgs_y // 2  # Center for array is half the image size
+    frequency_array = np.zeros((ts.height, ts.width))
+    cen_x = ts.width // 2  # Center for array is half the image size
+    cen_y = ts.height // 2  # Center for array is half the image size
 
-    rstep_x = 1 / (imgs_x * pixel_size)  # reciprocal pixel size
-    rstep_y = 1 / (imgs_y * pixel_size)
+    rstep_x = 1 / (ts.width * pixel_size)  # reciprocal pixel size
+    rstep_y = 1 / (ts.height * pixel_size)
 
     # Loop to fill array with frequency values
-    for x in range(imgs_x):
-        for y in range(imgs_y):
+    for x in range(ts.width):
+        for y in range(ts.height):
             d = np.sqrt(((x - cen_x) ** 2 * rstep_x**2) + ((y - cen_y) ** 2 * rstep_y**2))
-            frequency_array[x, y] = d
+            frequency_array[y, x] = d
 
     # Generate filtered stack
-    filtered_stack = np.zeros((imgs_x, imgs_y, n_tilt_imgs), dtype=np.single)
-    for i in range(n_tilt_imgs):
-        image = stack_data[:, :, i]
-        filtered_stack[:, :, i] = dose_filter_single_image(image, total_dose[i], frequency_array)
+    for z in range(ts.n_tilts):
+        image = ts.data[z, :, :]
+        ts.data[z, :, :] = dose_filter_single_image(image, total_dose[z], frequency_array)
 
-    if return_data_order == "zyx":
-        filtered_stack = filtered_stack.transpose(2, 1, 0)
+    ts.write_out(output_file)
 
-    if output_file is not None:
-        cryomap.write(filtered_stack, output_file)
-
-    return filtered_stack
+    return ts.correct_order()
 
 
-@suppress_warnings
 def dose_filter_single_image(image, dose, freq_array):
     # Hard-coded resolution-dependent critical exposures
     # These parameters come from the fitted numbers in the Grant and Grigorieff paper.
@@ -145,7 +215,9 @@ def deconvolve(
     highpass_nyquist=0.02,
     phase_flipped=False,
     phaseshift=0,
-    output_name=None,
+    output_file=None,
+    input_order="xyz",
+    output_order="xyz",
 ):
     """Deconvolution adapted from MATLAB script tom_deconv_tomo by D. Tegunov (https://github.com/dtegunov/tom_deconv)
     and adapted for the tilt series.
@@ -171,8 +243,12 @@ def deconvolve(
         whether the data are already phase-flipped. Defaults to False.
     phaseshift : int
         CTF phase shift in degrees (e. g. from a phase plate). Defaults to 0.
-    output_name : str
+    output_file : str
         Name of the output file for the deconvolved stack. Defaults to None (tilt stack will be not written).
+    input_order : str, default='xyz'
+        The order of axes in the input tilt stack. Defaults to xyz.
+    output_order : str, default='xyz'
+        The desired order of axes for the output tilt stacks. Defaults to xyz.
 
     Returns
     -------
@@ -180,17 +256,16 @@ def deconvolve(
         deconvolved tilt stack
 
     """
-    input_stack = cryomap.read(tilt_stack, transpose=False)
-    deconvolved_stack = np.zeros(input_stack.shape)
+    ts = TiltStack(tilt_stack=tilt_stack, input_order=input_order, output_order=output_order)
 
     if not isinstance(defocus, (int, float)):
         defocus = ioutils.defocus_load(defocus, defocus_file_type)
         defocus = defocus["defocus_mean"].values
     else:
-        defocus = np.full((input_stack.shape[0],), defocus)
+        defocus = np.full((ts.data.n_tilts,), defocus)
 
-    for ts in range(input_stack.shape[0]):
-        tilt = input_stack[ts, :, :]
+    for ts in range(ts.n_tilts):
+        tilt = ts.data[ts, :, :]
         interp_dim = np.maximum(2048, tilt.shape[0])
 
         # Generate highpass filter
@@ -237,29 +312,55 @@ def deconvolve(
 
         ramp = ramp_interp(r.flatten()).reshape(r.shape)
         # Perform deconvolution
-        deconvolved_stack[ts, :, :] = np.real(np.fft.ifftn(np.fft.fftn(tilt) * ramp))
+        ts.data[ts, :, :] = np.real(np.fft.ifftn(np.fft.fftn(tilt) * ramp))
 
-    if output_name is not None:
-        cryomap.write(deconvolved_stack, output_name, data_type=np.single, transpose=False)
+    ts.write_out(output_file)
 
-    return deconvolved_stack
+    return ts.correct_order()
 
 
-def split_stack_even_odd(tilt_stack):
+def split_stack_even_odd(tilt_stack, output_file_prefix=None, input_order="xyz", output_order="xyz"):
+    """Splits a given tilt stack into even and odd stacks.
+
+    Parameters
+    ----------
+    tilt_stack : str or array_like
+        The input stack of tilt images specified by its filename (including the path) or as 3d numpy array.
+    output_file_prefix : str, optional
+        The prefix for the output filenames. If provided, the function will save the even and odd stacks as files with
+        this prefix followed by '_even.mrc' and '_odd.mrc', respectively.
+    input_order : str, default='xyz'
+        The order of axes in the input tilt stack. Defaults to xyz.
+    output_order : str, default='xyz'
+        The desired order of axes for the output tilt stacks. Defaults to xyz.
+
+    Returns
+    -------
+    tuple of ndarray
+        A tuple containing two arrays: the first array contains the even indexed tilts, and the second array contains
+        the odd indexed tilts, both reordered according to `output_order`.
+
+    """
+
+    ts = TiltStack(tilt_stack=tilt_stack, input_order=input_order, output_order=output_order)
+
     even_stack = []
     odd_stack = []
 
     # For each tilt image in the stack
-    for i in range(0, tilt_stack.shape[2]):
+    for i in range(ts.n_tilts):
 
         # Split to even and odd by using modulo 2
         if i % 2 == 0:
-            even_stack.append(tilt_stack[:,:,i])
+            even_stack.append(ts.data[i, :, :])
         else:
-            odd_stack.append(tilt_stack[:,:,i])
+            odd_stack.append(ts.data[i, :, :])
 
-    # Stack separated tilts together into one volume
-    even_stack = np.stack(even_stack, axis=2)
-    odd_stack = np.stack(odd_stack, axis=2)
+    even_stack = np.stack(even_stack, axis=0)
+    odd_stack = np.stack(odd_stack, axis=0)
 
-    return even_stack, odd_stack
+    if output_file_prefix:
+        ts.write_out(output_file_prefix + "_even.mrc", new_data=even_stack)
+        ts.write_out(output_file_prefix + "_odd.mrc", new_data=odd_stack)
+
+    return ts.correct_order(even_stack), ts.correct_order(odd_stack)